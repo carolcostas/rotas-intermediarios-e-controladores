c32e0f49b1f9e07331a84d95265fe80ee56887fc
Carol <xcarol.costa@outlook.com>
2024-10-26 14:57:48 -0300

    adicionando parâmetros de rotas

--- src/servidor.ts
+++ src/servidor.ts
@@ -4,10 +4,11 @@ import  Express, { Request, Response }  from "express";
 const app = Express()
 
 const controlador = (req: Request, res: Response) => {
+    console.log(req.params.item)
     res.send("Hello World!")
 }
 
 
-app.get("/", controlador)
+app.get("/?:item", controlador)
 
 app.listen(process.env.PORT)
